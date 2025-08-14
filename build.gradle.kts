4bc36ff0947a3c6b8809f6af54081c6f29be53e1
Ralaba-cloud <adelaine75@gmail.com>
2025-08-14 18:35:58 +0200

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,1 +1,5 @@
-plugins { id("com.android.application") version "8.5.2" apply false }
+
+plugins {
+    id("com.android.application") version "8.5.2" apply false
+    kotlin("android") version "1.9.24" apply false
+}
